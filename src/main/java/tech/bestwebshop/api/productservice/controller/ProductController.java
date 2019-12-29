f8d6797de235f0cf041fdec6e52a3a0a9c0e7d6b
Wekra <github@cwernet.de>
2019-12-29 21:05:08 +0100

    Change endpoint names and return types

--- src/main/java/tech/bestwebshop/api/productservice/controller/ProductController.java
+++ src/main/java/tech/bestwebshop/api/productservice/controller/ProductController.java
@@ -1,6 +1,8 @@
 package tech.bestwebshop.api.productservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.bestwebshop.api.productservice.exception.ResourceNotFoundException;
 import tech.bestwebshop.api.productservice.model.Product;
@@ -8,6 +10,7 @@ import tech.bestwebshop.api.productservice.repositories.ProductRepository;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -16,18 +19,22 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
-    @GetMapping("/product")
-    public List<Product> getAllProducts(){
+    @GetMapping("/products")
+    public Iterable<Product> getAllProducts(){
         return productRepository.findAll();
     }
 
-    @GetMapping("/product/{id}")
-    public Product getProductById(@PathVariable(value="id") Integer productId) {
-        return productRepository.findById(productId)
-                .orElseThrow(() -> new ResourceNotFoundException("Product", "id", productId));
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable(value="id") Integer productId) {
+	System.out.println("#### Get Product by id " + productId);
+        Optional<Product> optionalProduct = productRepository.findById(productId);
+        return optionalProduct.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        //return productRepository.findById(productId)
+        //        .orElseThrow(() -> new ResourceNotFoundException("Product", "id", productId));
     }
 
-    @PostMapping("/product")
+    @PostMapping("/products")
     public Product createProduct(@Valid @RequestBody Product product){
         return productRepository.save(product);
     }
